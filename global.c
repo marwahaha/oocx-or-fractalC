6f597fed04169f8661a4df47567f79b44813b516
Roger Gomez Castells <imagina3124rogergc@gmail.com>
2013-03-04 12:45:35 +0100

    Update global.c

--- global.c
+++ global.c
@@ -1,8 +1,8 @@
 /*
-Copyright 2013 Roger Gómez Castells
-*/
-/*
-This program is free software: you can redistribute it and/or modify
+    file global.c from oocx project. set of flobal funcs to allow oo c programming as c++.
+    Copyright (C) 2013  Roger Gómez Castells
+
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
